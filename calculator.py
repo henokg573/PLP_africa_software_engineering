87827621c0417a826545bdf3e596252d97fecc47
Henok Girma <140075019+henokg573@users.noreply.github.com>
2025-07-29 18:28:07 +0300

    updated my calculator and folders too

--- calculator.py
+++
@@ -1,6 +0,0 @@
-# this is a comment
-# this is a calculator practice
-a = int(input("please enter the first number: "))
-b= int(input("please enter the second number number: "))
-sum = a+b
-print(sum)
